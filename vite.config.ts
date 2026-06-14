0c7412eadc4b438c7fbcb53d69f13ecc38db46ee
Ryan <catbe721@gmail.com>
2026-06-14 09:36:42 -0400

    fix github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/kids-bank/',
   plugins: [react()],
   build: {
     rollupOptions: {
